fb003dc23958c24f6a304e0747f4523eff560deb
Álvaro Luis Vieira Quevedo Júnior <alvaroquevedo@live.com>
2019-07-17 10:27:13 -0300

    intial commit

---
+++ HighResolutionTimer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace MicroTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = Process.GetCurrentProcess();
+            p.PriorityBoostEnabled = true;  // every little helps ;)
+            p.PriorityClass = ProcessPriorityClass.RealTime;
+
+            Console.WriteLine("Priviledged processor time for process " + p.ProcessName + " is " + p.PrivilegedProcessorTime.TotalMilliseconds.ToString("#,##0.0") + " ms");
+            Console.WriteLine("Number of ticks per second: " + Stopwatch.Frequency.ToString("#,##0"));
+
+            UseMicroTimerClass();
+            Console.ReadLine();
+        }
+
+        #region MicroTimer
+        private static void UseMicroTimerClass()
+        {
+            Console.WriteLine("MICRO TIMER CLASS:");
+            HighResolutionTimer lMicroTimer = new HighResolutionTimer(50, 3);
+            lMicroTimer.OnTimer += OnTimer;
+            lMicroTimer.OnStoped += OnStoped;
+            lMicroTimer.OnSkipped += OnSkipped;
+            lMicroTimer.Start();
+        }
+
+        static void OnSkipped(object sender, EventArgs args)
+        {
+            Console.WriteLine($"Cycle skipped");
+        }
+
+        static void OnStoped(object sender, EventArgs args)
+        {
+            Console.WriteLine("Cycle stopped.");
+        }
+
+        static void OnTimer(object sender, EventArgs args)
+        {
+            Console.WriteLine($"Cycle timer");
+        }
+        #endregion
+    }
+}
